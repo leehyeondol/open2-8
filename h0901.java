126fbd2cb346b36668d5b360dab42d9ecd06adc9
Hyeon-Do Lee <48902147+leehyeondol@users.noreply.github.com>
2022-11-01 17:22:54 +0900

    Add files via upload

---
+++ h0901.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class h0901 extends JFrame {
+    h0901() {
+        setTitle("Let's study Java");
+        setSize(400, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new h0901();
+    }
+}
